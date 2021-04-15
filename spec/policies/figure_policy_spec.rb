cc2bdc041adeed7810ac7e047aa02a06c812dc3e
Alexander Trieschmann <info@ali-trieschmann.de>
2021-04-15 16:53:41 +0200

    Refactor code formatting of spec/policies
    Using rubocop for rails

--- spec/policies/figure_policy_spec.rb
+++ spec/policies/figure_policy_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe FigurePolicy, type: :policy do
   it_behaves_like 'inventory policies'
 end
--- spec/policies/location_policy_spec.rb
+++ spec/policies/location_policy_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.describe LocationPolicy, type: :policy do
   it_behaves_like 'inventory policies'
 end
